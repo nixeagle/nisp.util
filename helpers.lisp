12008d4d0f9157be62f33241c926045ee6b2e0a7
James S <i@nixeagle.org>
2010-01-20 04:08:42 +0000

    add 3 timing macros

--- helpers.lisp
+++ helpers.lisp
@@ -1,7 +1,8 @@
 (in-package :nisp.util-system)
 (defpackage #:nisp.util-helpers
-  (:use :common-lisp :iterate)
-  (:nicknames :h :helpers))
+  (:use :common-lisp :iterate :nisp.util-types)
+  (:nicknames :h :helpers)
+  (:shadow :time))
 (in-package :nisp.util-helpers)
 
 (defun export-some-internals (package-name)
@@ -18,3 +19,18 @@
              :having-access (:external))
         (collect symbol)
         (unintern symbol from-package)))
+
+(defmacro time (count &body body)
+  `(cl:time (iterate (for ,(gensym) :from 0 :to ,count)
+                     ,@body)))
+(defmacro time-to-string (count &body body)
+  (declare (type (integer 0 10000000000) count)
+           (optimize (debug 3) (safety 3) (speed 0) (space 0)))
+  `(with-output-to-string (*trace-output*)
+     (time ,count ,@body)))
+
+(defmacro time-to-string-optimized (count &body body)
+  (declare (type (integer 0 10000000000) count)
+           (optimize (debug 0) (safety 0) (speed 3) (space 1)))
+  `(with-output-to-string (*trace-output*)
+     (time ,count ,@body)))
