15d27ee1eee3cea590cce88c4997e9614afbef43
James S <i@nixeagle.org>
2010-01-20 05:41:50 +0000

    add clock helper macro

--- helpers.lisp
+++ helpers.lisp
@@ -20,6 +20,7 @@
         (collect symbol)
         (unintern symbol from-package)))
 
+
 (defmacro time (count &body body)
   `(cl:time (iterate (for ,(gensym) :from 0 :to ,count)
                      ,@body)))
@@ -34,3 +35,34 @@
            (optimize (debug 0) (safety 0) (speed 3) (space 1)))
   `(with-output-to-string (*trace-output*)
      (time ,count ,@body)))
+
+(defmacro clock (form &optional (iterations 10000))
+  "Execute FORM ITERATIONS times.
+
+The result is an alist of the form:
+  ((time . rational) (iterations . integer) (result . <form result>))"
+  ;; This macro has a bug, it evaluates FORM one extra time at the end
+  ;; of the macro expression to display the result value.
+  (declare (type positive-fixnum)
+           (optimize (debug 0) (speed 3) (safety 0)
+                     (space 1) (compilation-speed 3)))
+  (let ((start (gensym))
+        (total (gensym))
+        (avg (gensym)))
+    `(iter (with ,start = (get-internal-real-time))
+            (for ,(gensym) :from 0 :to ,iterations)
+            ,form
+            (finally (let* ((,total (the (or (integer 0 0) positive-fixnum) 
+                                      (- (get-internal-real-time) ,start)))
+                            (,avg (/ ,total 
+                                     (* ,iterations
+                                        internal-time-units-per-second))))
+                       (return 
+                         (list 
+                          (cons :time (list 
+                                       (/ ,total internal-time-units-per-second)
+                                       (format nil "~8f" 
+                                               (/ ,total 
+                                                  internal-time-units-per-second))))
+                          (cons :avg (list ,avg (format nil "~8f" ,avg)))
+                          (cons :result ,form))))))))
