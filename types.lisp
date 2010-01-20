d42f001740be71469dcca395d5f7bca95dc6810f
James S <i@nixeagle.org>
2010-01-20 00:22:09 +0000

    initial import of types

---
+++ types.lisp
@@ -0,0 +1,23 @@
+(in-package :nisp.util-system)
+(defpackage #:nisp.util-types
+  (:use :common-lisp :iterate)
+  (:export #:type-specifier-p
+           type-specifier
+           structure-designator))
+(in-package :nisp.util-types)
+
+;; Idea for (documentation symbol 'type) taken from slime.lisp
+(defun type-specifier-p (symbol)
+  "True if SYMBOL is a type."
+  (or (documentation symbol 'type)
+      #+:sbcl
+      (multiple-value-bind (arglist exists) 
+          (sb-introspect:deftype-lambda-list symbol)
+        (values exists arglist))))
+
+(deftype type-specifier ()
+  "Type specifier that can be used with TYPEP"
+  '(satisfies type-specifier-p))
+
+(deftype structure-designator ()
+  '(or symbol structure-class structure-object))
